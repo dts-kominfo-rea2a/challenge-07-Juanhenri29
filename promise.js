96784b9af88ec2ce2cf01ea729f9c18900dedef5
Juan Sianturi <lioneljuansianturi29@gmail.com>
2022-06-12 20:07:07 +0700

    feat: file promise.js sudah dikerjakan

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const fs = require('fs/promises')
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const dataFile = './external.js';
+const utf8 = 'utf-8';
+
+const promiseOutput = async (args) => {
+
+  try {
+    let hasil= [];
+    const hasilIXX = await promiseTheaterIXX(fs.readFile(dataFile, utf8))
+    const hasilVGC = await promiseTheaterVGC(fs.readFile(dataFile, utf8))
+    hasil.push(hasilIXX)
+    hasil.push(hasilVGC)
+
+    if (args === 'marah') {
+      const hitungHasilIXX = hasil[0].filter((a) => a.hasil == 'marah')
+      const hitungHasilVGC = hasil[1].filter((a) => a.hasil == 'marah')
+      return hitungHasilIXX.length + hitungHasilVGC.length
+    }
+    else {
+      const hitungHasilIXX = hasil[0].filter((a) => a.hasil == 'tidak marah')
+      const hitungHasilVGC = hasil[1].filter((a) => a.hasil == 'tidak marah')
+      return hitungHasilIXX.length + hitungHasilVGC.length
+    }
+  }
+  catch(err) {
+    return err
+  }
+};
 
 module.exports = {
   promiseOutput,
